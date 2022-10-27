2f59a925ccc4a95a77992d09cc3fe01f60ff9e5c
이한솔 <hansol8701@naver.com>
2022-10-27 16:28:17 +0900

    refactor

--- baekjoon_algorithm/DP/2293_동전1.py
+++ baekjoon_algorithm/DP/2293_동전1.py
@@ -1,24 +1,4 @@
-# import sys
-#
-# n, k = map(int, sys.stdin.readline().rstrip().split())
-# coins = []
-# dp = [[0 for j in range(n)] for i in range(k + 1)]
-#
-# for i in range(n):
-#     coins.append(int(sys.stdin.readline().rstrip()))
-#
-# for i in range(n):
-#     dp[coins[i]][i] = 1
-#
-# for i in range(k + 1):
-#     for j in range(n):
-#         if i - coins[j] >= 1:
-#             sum_ = 0
-#             for m in range(j, n):
-#                 sum_ += dp[i - coins[j]][m]
-#             dp[i][j] = sum_
-#
-# print(sum(dp[k]))
+
 
 import sys
 
