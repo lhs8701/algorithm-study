69f9c40fb25b9ae83b16a2a6c323d2de999eeb17
이한솔 <hansol8701@naver.com>
2023-02-15 21:40:14 +0900

    add 2643

--- baekjoon_algorithm/Sorting/2643_색종이_올려_놓기.py
+++ baekjoon_algorithm/Sorting/2643_색종이_올려_놓기.py
@@ -17,21 +17,35 @@ for i in range(1, N):
     cnt[i] = max_val + 1
 print(max(cnt))
 
-# import sys
-#
-# N = int(sys.stdin.readline().rstrip())
-# temp = []
-# for _ in range(N):
-#     a, b = map(int, sys.stdin.readline().rstrip().split())
-#     temp.append((min(a, b), max(a, b)))
-#
-# temp.sort()
-# arr = [elem[1] for elem in temp]
-# cnt = [1 for i in range(N)]
-# for i in range(1, N):
-#     for j in range(i - 1, -1, -1):
-#         if arr[j] <= arr[i]:
-#             cnt[i] = cnt[j] + 1
-#             break
-# print(cnt)
-# print(max(cnt))
+# upper_bound 이용
+
+import sys
+
+
+def upper_bound(lis, key, right):
+    left = 0
+    while left < right:
+        mid = (left + right) // 2
+        if lis[mid] <= key:
+            left = mid + 1
+        else:
+            right = mid
+    return left
+
+
+N = int(sys.stdin.readline().rstrip())
+temp = []
+for _ in range(N):
+    a, b = map(int, sys.stdin.readline().rstrip().split())
+    temp.append((min(a, b), max(a, b)))
+temp.sort()
+arr = [elem[1] for elem in temp]
+lis = [arr[0]]
+for i in range(1, N):
+    idx = upper_bound(lis, arr[i], len(lis))
+    if idx == len(lis):
+        lis.append(arr[i])
+    else:
+        lis[idx] = arr[i]
+
+print(len(lis))
