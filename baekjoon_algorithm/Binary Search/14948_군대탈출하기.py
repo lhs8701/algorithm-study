53f55a7981eca3ac722dd442269cad6a312f281f
이한솔 <hansol8701@naver.com>
2023-04-13 00:59:01 +0900

    add 14948

--- baekjoon_algorithm/Binary Search/14948_군대탈출하기.py
+++ baekjoon_algorithm/Binary Search/14948_군대탈출하기.py
@@ -1,40 +1,35 @@
 import sys
+from collections import deque
 
-sys.setrecursionlimit(10000)
 
-
-def check(limit, cur_row, cur_col, visited, use):
-    if cur_row == n - 1 and cur_col == m - 1:
-        return True
-
-    visited[cur_row][cur_col] = True
-    for d_row, d_col in dir:
-        next_row, next_col = cur_row + d_row, cur_col + d_col
-        if 0 <= next_row < n and 0 <= next_col < m and not visited[next_row][next_col] and matrix[next_row][
-            next_col] <= limit:
-            result = check(limit, next_row, next_col, visited, use)
-            if result:
-                return True
-            visited[next_row][next_col] = False
-    if not use:
+def bfs(level):
+    visited = [[[False, False] for _ in range(m)] for _ in range(n)]  # [찬스 안씀, 찬스 씀]
+    dir = [(+1, 0), (-1, 0), (0, +1), (0, -1)]
+    if matrix[0][0] > level:
+        return False
+    queue = deque([(0, 0, 0)])
+    visited[0][0][0] = True
+    while queue:
+        v_row, v_col, used = queue.popleft()
         for d_row, d_col in dir:
-            next_row, next_col = cur_row + 2 * d_row, cur_col + 2 * d_col
-            if 0 <= next_row < n and 0 <= next_col < m and not visited[next_row][next_col] and matrix[next_row][
-                next_col] <= limit:
-                result = check(limit, next_row, next_col, visited, True)
-                if result:
-                    return True
-                visited[next_row][next_col] = False
-
-    return False
+            u_row, u_col = v_row + d_row, v_col + d_col
+            if 0 <= u_row < n and 0 <= u_col < m and not visited[u_row][u_col][used]:
+                if matrix[u_row][u_col] <= level:
+                    visited[u_row][u_col][used] = True
+                    queue.append((u_row, u_col, used))
+                else:
+                    if used == 0 and 0 <= u_row + d_row < n and 0 <= u_col + d_col < m and matrix[u_row + d_row][
+                        u_col + d_col] <= level:
+                        visited[u_row + d_row][u_col + d_col][1] = True
+                        queue.append((u_row + d_row, u_col + d_col, 1))
+    return visited[n - 1][m - 1][0] or visited[n - 1][m - 1][1]
 
 
 def binary_search(left, right):
-    ans = -1
+    ans = 0
     while left <= right:
         mid = (left + right) // 2
-        visited = [[False for _ in range(m)] for _ in range(n)]
-        if check(levels[mid], 0, 0, visited, False):
+        if bfs(levels[mid]):
             ans = levels[mid]
             right = mid - 1
         else:
@@ -45,7 +40,6 @@ def binary_search(left, right):
 n, m = map(int, sys.stdin.readline().rstrip().split())
 
 matrix = []
-dir = [(-1, 0), (0, +1), (+1, 0), (0, -1)]
 level_set = set()
 for i in range(n):
     temp = list(map(int, sys.stdin.readline().rstrip().split()))
@@ -53,5 +47,4 @@ for i in range(n):
     level_set.update(set(temp))
 levels = list(level_set)
 levels.sort()
-
 print(binary_search(0, len(levels) - 1))
