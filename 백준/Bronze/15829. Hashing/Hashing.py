e717f2b35a242f494024a347f1ef969ad49b8984
HANSOL <45627010+lhs8701@users.noreply.github.com>
2023-04-16 21:44:44 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -5,6 +5,6 @@ str = sys.stdin.readline().rstrip()
 sum = 0
 for i in range(L):
     idx = ord(str[i]) - 96
-    sum += idx * (31 ** i) % 1234567891
+    sum += (idx * ((31 ** i) % 1234567891)) % 1234567891
 
 print(sum)
