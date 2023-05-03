3953f52531dd72156aa14a00ff3ceed9f3a3f8f4
HANSOL <45627010+lhs8701@users.noreply.github.com>
2023-05-03 17:56:21 +0900

    [unrated] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -0,0 +1,12 @@
+
+# SELECT info.*, plan.*, floor(info.DAILY_FEE * (100 - plan.DISCOUNT_RATE) / 100 * 30) as FEE
+SELECT info.CAR_ID, info.CAR_TYPE, floor(info.DAILY_FEE * (100 - plan.DISCOUNT_RATE) / 100 * 30) as FEE
+from CAR_RENTAL_COMPANY_CAR as info 
+inner join CAR_RENTAL_COMPANY_DISCOUNT_PLAN as plan on info.CAR_TYPE = plan.CAR_TYPE and DURATION_TYPE = '30일 이상'
+where (info.CAR_TYPE = '세단' or info.CAR_TYPE = 'SUV')
+    and info.CAR_ID not in (select CAR_ID from CAR_RENTAL_COMPANY_RENTAL_HISTORY as history
+                        where   (history.START_DATE < '2022-11-01' and '2022-11-30' < history.END_DATE) or
+                                (history.START_DATE between '2022-11-01' and '2022-11-30') or
+                                (history.END_DATE between '2022-11-01' and '2022-11-30'))
+HAVING FEE >= 500000 AND FEE < 2000000
+order by FEE DESC, info.CAR_TYPE ASC, info.CAR_ID desc;
