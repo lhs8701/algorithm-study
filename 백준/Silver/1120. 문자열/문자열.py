5ebfb2da012343a5579978d7f3e5cf84764c2378
HANSOL <45627010+lhs8701@users.noreply.github.com>
2023-09-05 20:25:06 +0900

    [Silver IV] Title: 문자열, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/1120. 문자열/문자열.py
@@ -0,0 +1,13 @@
+# 1120번
+
+a, b = input().split()
+
+answer = []
+for i in range(len(b) - len(a) + 1):
+    count = 0
+    for j in range(len(a)):
+        if a[j] != b[i + j]:
+            count += 1
+    answer.append(count)
+
+print(min(answer))
