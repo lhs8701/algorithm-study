18cc922feb4459cac519b381e7fc753f080df24f
HANSOL <45627010+lhs8701@users.noreply.github.com>
2023-09-16 03:34:41 +0900

    [level 0] Title: 배열의 유사도, Time: 0.17 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        Set<String> set1 = new HashSet<>(Arrays.asList(s1));
+        Set<String> set2 = new HashSet<>(Arrays.asList(s2));
+        set1.retainAll(set2);
+        int answer = set1.size();
+        return answer;
+    }
+}
