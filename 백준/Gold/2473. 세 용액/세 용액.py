82d6acf6e16ec4cc054fe9d4acd0a4e8b91c57a5
HANSOL <45627010+lhs8701@users.noreply.github.com>
2023-09-16 00:52:17 +0900

    [Gold III] Title: 세 용액, Time: 476 ms, Memory: 116256 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.py
+++ 백준/Gold/2473. 세 용액/세 용액.py
@@ -1,9 +1,10 @@
 import sys
 
 
-def twoPointer(numbers, left, pivot):
-    right = len(numbers) - 1
-    ans = [int(1e10), int(1e10), int(1e10)]
+def twoPointer(numbers, pivot):
+    left, right = 0, len(numbers) - 1
+    target = -pivot
+    ans = [int(1e10), int(1e10)]
     while left < right:
         if numbers[left] == pivot:
             left += 1
@@ -12,26 +13,26 @@ def twoPointer(numbers, left, pivot):
             right -= 1
             continue
 
-        if abs(numbers[left] + numbers[right] + pivot) < abs(sum(ans)):
-            ans[0], ans[1], ans[2] = pivot, numbers[left], numbers[right]
+        val = numbers[left] + numbers[right]
+        if abs(target - val) < abs(target - (ans[0] + ans[1])):
+            ans[0], ans[1] = numbers[left], numbers[right]
 
-        if numbers[left] + numbers[right] + pivot < 0:
-            left += 1
-        else:
+        if val > target:
             right -= 1
-    return ans
-
+        else:
+            left += 1
+    return ans[0], ans[1]
 
 N = int(sys.stdin.readline().rstrip())
 numbers = list(map(int, sys.stdin.readline().rstrip().split()))
 numbers.sort()
 
-finalAns = [1e10, 1e10, 1e10]
-for i in range(N - 1):
-    pivot = numbers[i]
-    num1, num2, num3 = twoPointer(numbers, i + 1, pivot)
-    if abs(num1 + num2 + num3) < abs(sum(finalAns)):
-        finalAns[0], finalAns[1], finalAns[2] = num1, num2, num3
 
-finalAns.sort()
-print(finalAns[0], finalAns[1], finalAns[2])
+ans = [1e10, 1e10, 1e10]
+for pivot in numbers:
+    num1, num2 = twoPointer(numbers, pivot)
+    if abs(num1 + num2 + pivot) < abs(sum(ans)):
+        ans[0], ans[1], ans[2] = num1, num2, pivot
+
+ans.sort()
+print(ans[0], ans[1], ans[2])
