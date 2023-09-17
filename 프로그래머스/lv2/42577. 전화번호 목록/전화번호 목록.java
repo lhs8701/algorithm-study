57e8f0529d6d666fc44e9566ec57434290873a72
HANSOL <45627010+lhs8701@users.noreply.github.com>
2023-09-17 21:00:41 +0900

    [level 2] Title: 전화번호 목록, Time: 331.25 ms, Memory: 96.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+        for(int i=0; i<phone_book.length - 1; i++){
+            if (phone_book[i+1].substring(0, Math.min(phone_book[i].length(), phone_book[i+1].length())).equals(phone_book[i])){
+                return false;
+            }
+        }
+        return true;
+    }
+}
