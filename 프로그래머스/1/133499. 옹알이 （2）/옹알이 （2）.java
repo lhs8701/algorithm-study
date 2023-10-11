2dc3a4823a1d5a888c0678bd3f34fdd75c2882da
HANSOL <45627010+lhs8701@users.noreply.github.com>
2023-10-11 11:44:51 +0900

    [level 1] Title: 옹알이 (2), Time: 1.24 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,34 +1,6 @@
-import java.util.*;
-import java.util.stream.*;
-
 class Solution {
-    String[] words = new String[]{"aya", "ye", "woo", "ma"};
     public int solution(String[] babbling) {
-        return Arrays.stream(babbling)
-            .mapToInt(bab -> func(bab))
-            .sum();
-    }
-    
-    int func(String str){
-        int prev = -1;
-        while (!str.isBlank()){
-            boolean success = false;
-            for(int i=0; i<4; i++){
-                if (i == prev){
-                    continue;
-                }
-                String word = words[i];
-                if (str.substring(0, Math.min(word.length(), str.length())).equals(word)){
-                    str = str.substring(word.length());
-                    prev = i;
-                    success = true;
-                    break;
-                }
-            }
-            if (!success){
-                return 0;
-            }
-        }
-        return 1;
+        int answer = 0;
+        return answer;
     }
 }
